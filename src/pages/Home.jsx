e576b8289096e2c395f44c7db0f581b63f7a5242
Abiodun <abiodunhodonu@gmail.com>
2023-07-21 23:54:01 +0100

    feature section

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,16 +1,38 @@
 import React from "react";
 import Footer from "../components/Footer";
 import Navbar from "../components/Navbar";
+import house from '../assets/home.png'
+import vintage from "../assets/feature1.png"
 
 const Home = () => {
   return (
-    <div>
-      <Navbar />
-        <section className="container">
-            <h3>Perfect Way To Buy And Sell a House</h3>
+      <div>
+        <Navbar />
+
+        <header className="container header__container">
+            <div className="header__content">
+            <h1>Perfect Way To Buy And Sell a House</h1>
             <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. 
                 Quam laborum sequi voluptate.
+                Lorem ipsum dolor sit amet consectetur adipisicing elit. 
+                Quam laborum sequi voluptate.
             </p>
+            </div>
+            <div className="header__image">
+                <img src={house} alt="houes" />
+            </div>
+        </header>
+       
+        <section className="container">
+            <h2>Featured In</h2>
+            <div className="feature__image">
+                <img src={vintage} alt="" />
+                <img src={vintage} alt="" />
+                <img src={vintage} alt="" />
+                <img src={vintage} alt="" />
+                <img src={vintage} alt="" />
+            </div>
+           
         </section>
         <Footer />
     </div>
