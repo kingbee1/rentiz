d58a9c22fd51d3945c0c4a8ae3e21f413fc089d9
Abiodun <abiodunhodonu@gmail.com>
2023-07-29 20:01:46 +0100

    login page

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,15 +1,30 @@
 import React from 'react'
 import Footer from '../components/Footer'
 import Navbar from '../components/Navbar'
+import login from '../assets/house2.png'
+import '../styles/login.css'
 
 const Login = () => {
   return (
     <div>
       <Navbar />
-      <h1>Login</h1>
-      <h1>Login</h1>
-      <h1>Login</h1>
-      <h1>Login</h1>
+      <div className='container login__container'>
+       
+        <div className='login__container-img'>
+          <img src={login}/>
+        </div>
+
+        <div className='login__container-form'>
+          <h1>Login</h1>
+          <form className='form__container'>
+            <label for="email">Email address</label>
+            <input type="email" name="" id="" />
+            <label for="password">Password</label> 
+            <input type="password" name="" required/> 
+            <a className='form__btn' href="/register">Login</a>
+          </form>
+        </div>
+      </div>
       <Footer />
     </div>
   )
--- src/pages/Register.jsx
+++ src/pages/Register.jsx
@@ -1,15 +1,32 @@
 import React from 'react'
 import Navbar from '../components/Navbar'
 import Footer from '../components/Footer'
+import login from '../assets/home.png'
 
 const Register = () => {
   return (
     <div>
       <Navbar />
-      <h1>Register</h1>
-      <h1>Register</h1>
-      <h1>Register</h1>
-      <h1>Register</h1>
+      <div className='container login__container'>
+       
+        <div className='login__container-img'>
+          <img src={login}/>
+        </div>
+
+        <div className='login__container-form'>
+          <h1>Register</h1>
+          <form className='form__container'>
+            <label for="email">Email address</label>
+            <input type="email" name="" id="" />
+            <label for="password">Password</label> 
+            <input type="password" name="" required/> 
+            <div>
+            <a className='form__btn' href="/register">Create Account</a>
+            <a className='form__btn' href="/register">Login</a>
+            </div>
+          </form>
+        </div>
+      </div>
       <Footer />
     </div>
   )
