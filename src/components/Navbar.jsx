7b03fda6c6ebf792dfe457e785de51bed45d455e
Abiodun <abiodunhodonu@gmail.com>
2023-09-06 13:42:01 +0100

    fixed nav items for big screens

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -15,6 +15,15 @@ const Navbar = () => {
     <nav>
       <div className="container nav__container">
         <Link to={"/"} className="nav__container-logo logo">Rentiz</Link>
+
+        <ul className='nav__items-a'>
+          <NavLink to='/'><li>Home</li></NavLink>
+          <NavLink to="/about-us"><li>About Us</li></NavLink>
+          <NavLink to='/buying'><li>Buying</li></NavLink>
+          <NavLink to='/renting'><li>Renting</li></NavLink>
+          <NavLink to='/selling'><li>Selling</li></NavLink>
+          <NavLink to='/contact-us'><li>Contact Us</li></NavLink>
+        </ul>
        
         <ul className={click ? 'nav__items' : 'menu'}>
           <NavLink to='/'><li>Home</li></NavLink>
